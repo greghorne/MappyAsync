d86603d8bd0c61d4b5d077edf6b538d1945a9305
greghorne <greghorne@hotmail.com>
2018-07-12 20:01:03 +0000

    updated browser tab title and made adjustments to map postition and zoom

--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -1,7 +1,3 @@
-Leaflet.tile_layer =  "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-
-# Leaflet.tile_layer = "http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png"
-# Leaflet.subdomains = ['otile1', 'otile2', 'otile3', 'otile4']
-
+Leaflet.tile_layer  =  "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 Leaflet.attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-Leaflet.max_zoom   = 18
+Leaflet.max_zoom    = 18
