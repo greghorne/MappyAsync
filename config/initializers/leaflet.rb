86b36edc32c0205f75cab1cffaa0c6aaa26a703f
greghorne <greghorne@hotmail.com>
2018-07-12 19:33:40 +0000

    updated css and erb; base map is now up

--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -3,5 +3,5 @@ Leaflet.tile_layer =  "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 # Leaflet.tile_layer = "http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png"
 # Leaflet.subdomains = ['otile1', 'otile2', 'otile3', 'otile4']
 
-Leaflet.attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+Leaflet.attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
 Leaflet.max_zoom   = 18
