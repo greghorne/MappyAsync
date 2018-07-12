021db8854de66aa7ee7c7fac9bafa94ec766cadd
greghorne <greghorne@hotmail.com>
2018-07-12 19:22:45 +0000

    worked on generating base map; added help file and js

--- app/models/mappyasync.rb
+++ app/models/mappyasync.rb
@@ -2,7 +2,7 @@ require "resolv"
 # require "redis"
 # require "redis-namespace"
 
-$redis = Redis::Namespace.new("redis_hostnames", :redis => Redis.new)
+# $redis = Redis::Namespace.new("redis_hostnames", :redis => Redis.new)
 
 class MappyAsyncMain
 
---
+++ config/initializers/leaflet.rb
@@ -0,0 +1,7 @@
+Leaflet.tile_layer =  "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+
+# Leaflet.tile_layer = "http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png"
+# Leaflet.subdomains = ['otile1', 'otile2', 'otile3', 'otile4']
+
+Leaflet.attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+Leaflet.max_zoom   = 18
