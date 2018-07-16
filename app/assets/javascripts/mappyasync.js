444f2b7badf977784e6385bb7c4fef24e062ce95
greghorne <greghorne@hotmail.com>
2018-07-16 15:15:40 +0000

    code updated; finished changed from leaflet gem to leaflet js

--- app/assets/javascripts/mappyasync.js
+++ app/assets/javascripts/mappyasync.js
@@ -4,6 +4,13 @@
 
 $(document).ready(function() {
 
+    var map = L.map('map').setView([39.5, -98.35], 5)
+    
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}, detectRetina=true', {
+        attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
+        max_zoom:    16
+    }).addTo(map)
+
     map.on('click', function(event) {
         // console.log("lat: " + event.latlng.lat + ", lng: " + event.latlng.lng);
         $.ajax({url: "/mapclick", data: { 'lat': event.latlng.lat, 'lng': event.latlng.lng}}
--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -2,6 +2,6 @@
 # Leaflet.attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
 # Leaflet.max_zoom    = 18
 
-Leaflet.tile_layer   = 'https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}, detectRetina=true'
-Leaflet.attribution  = 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC'
-Leaflet.max_zoom     = 16
+# Leaflet.tile_layer   = 'https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}, detectRetina=true'
+# Leaflet.attribution  = 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC'
+# Leaflet.max_zoom     = 16
