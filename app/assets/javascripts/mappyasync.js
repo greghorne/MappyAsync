eb5551a972628b8143d9960af55acebd96e30f4c
greghorne <greghorne@hotmail.com>
2018-07-16 20:09:14 +0000

    adjusted code for flyTo and removal of any markers that are already on the map

--- app/assets/javascripts/mappyasync.js
+++ app/assets/javascripts/mappyasync.js
@@ -8,16 +8,19 @@ $(document).ready(function() {
     // define base map layer
     var grayscale = L.tileLayer("http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png", {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-        maxZoom: 18
+        minZoon: 4,
+        maxZoom: 16
     });
 
     var esri = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}, detectRetina=true', {
         attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
+        minZoom: 4,
         maxZoom: 16
     });
 
     var osm = L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+        minZoom: 2,
         maxZoom: 18
     });
     ////////////////////////////////////////////////////////////
@@ -46,12 +49,15 @@ $(document).ready(function() {
 
     ////////////////////////////////////////////////////////////
     // add geocoder
+    var marker
+    
     var geocoder = L.Control.geocoder({
-        defaultMarkGeocode: false
-    }).on('markgeocode', function(e) {
-        console.log(e.geocode.center.lat)
-        map.flyTo([e.geocode.center.lat, e.geocode.center.lng], 14)
-
+            defaultMarkGeocode: false
+        }).on('markgeocode', function(e) {
+            map.flyTo([e.geocode.center.lat, e.geocode.center.lng], 14)
+            if (marker) map.removeLayer(marker);
+            marker = new L.marker([e.geocode.center.lat, e.geocode.center.lng]) //.addTo(map)
+            map.addLayer(marker);
     }).addTo(map)
     ////////////////////////////////////////////////////////////
     
@@ -74,6 +80,11 @@ $(document).ready(function() {
                     .setContent("Clicked: " + event.latlng.toString())
                     .openOn(map)
         });
+    });
+
+    map.on('zoomlevelschange', function(event) {
+        console.log("zoom changed");
+        console.log(event);
     });
     
 })
