3614099b4135615e60461645aacdc0c4446396d1
greghorne <greghorne@hotmail.com>
2018-07-16 17:15:05 +0000

    adjusted base map layers

--- app/assets/javascripts/mappyasync.js
+++ app/assets/javascripts/mappyasync.js
@@ -4,12 +4,37 @@
 
 $(document).ready(function() {
 
-    var map = L.map('map').setView([39.5, -98.35], 5)
-    
-    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}, detectRetina=true', {
+   
+    var esri = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}, detectRetina=true', {
         attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
-        max_zoom:    16
-    }).addTo(map)
+        maxZoom: 16
+    });
+
+    var osm = L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+        maxZoom: 18
+    });
+
+    var grayscale = L.tileLayer("http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png", {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+        maxZoom: 18
+    });
+
+    var map = L.map('map', {
+        center: [39.5, -98.35],
+        zoom: 5,
+        layers: [grayscale]
+    });
+
+    var baseMaps = {
+        "Grayscale": grayscale,
+        "Esri": esri,
+        "OSM": osm
+    }
+
+    L.control.layers(baseMaps).addTo(map)
+
+
 
     map.on('click', function(event) {
         // console.log("lat: " + event.latlng.lat + ", lng: " + event.latlng.lng);
--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module Code
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # config.assets.precompile += %w(assets/images/*)
+    # config.assets.paths << Rails.root.join("images", "/vendor/assets/images")
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
