6751ec0e4fac070c8108c6dabe2c19a50f1dba3a
greghorne <greghorne@hotmail.com>
2018-08-16 23:38:41 +0000

    removed trace statements

--- app/assets/javascripts/mappyasync.js
+++ app/assets/javascripts/mappyasync.js
@@ -6,7 +6,7 @@
 ////////////////////////////////////////////////////////////
 const CONST_OSM_REVERSE_GEOCODING = true; // GOOGLE if false
 
-const CONST_MAP_CLICK_MIN_ZOOM = 12;
+const CONST_MAP_CLICK_MIN_ZOOM = 15;
 
 // default map settings
 // const CONST_MAP_DEFAULT_LONGITUDEX = -98.35;
@@ -18,32 +18,48 @@ const CONST_MAP_DEFAULT_ZOOM       =   15;
 // OSM reverse geocoder
 const CONST_OSM_URL             = "https://nominatim.openstreetmap.org/reverse";
 const CONST_OSM_FORMAT          = "jsonv2";
-const CONST_OSM_GEOCODE_ZOOM    = 18;
+const CONST_OSM_GEOCODE_ZOOM    = 15;
 const CONST_OSM_ADDR_DETAILS    =  1;
 
-// defintion of map layers; first layer is the default layer displayed
+// definition of map layers; first layer is the default layer displayed
 const CONST_MAP_LAYERS = [
     {
-        name: "Grayscale",
-        url: "http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png",
+        // not https so can generate warnings due to mixed content
+        // 2018-08-12 - https site currently has a NET::ERR_CERT_COMMON_NAME_INVALID
+        name: "Grayscale OSM",
+        url: "http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png",      
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
         minZoom:  5,
         maxZoom: 17
     },
     {
-        name: "Esri",
-        url: 'https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}, detectRetina=true',
-        attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
+        name: "Esri OSM",
+        url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
         minZoom:  5,
         maxZoom: 17
     },
     {
-        name: "OSM",
-        url: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-        attirbution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+        name: "Hydda OSM",
+        url: "https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png",
+        attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
         minZoom:  5,
         maxZoom: 17
     },
+    {
+        name: "Basic OSM",
+        url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+        minZoom:  5,
+        maxZoom: 17
+    },
+    {
+        name: "Esri World Imagery",
+        url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+        minZoom:  5,
+        maxZoom: 17
+    }
 ];
 ////////////////////////////////////////////////////////////
 
@@ -118,7 +134,6 @@ function initCustomButton(map, classString, toolTip, fn) {
             var container = L.DomUtil.create('div', classString + ' sidebar-icon button-custom cursor-pointer leaflet-bar');
             container.title   = toolTip
             container.onclick = function(e){ 
-                console.log("click..."); 
                 L.DomEvent.stopPropagation(e);
                 fn();
             }
@@ -238,7 +253,6 @@ $(document).ready(function() {
     initGeocoder(map)
 
     function sidebarOpenClose() { 
-        console.log("fn...")
         if (sidebar.isVisible()) {
             sidebar.hide();
         } else {
