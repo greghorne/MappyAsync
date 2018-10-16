8dbada062d2c22b1d87f519845a34ebb4c2b1d82
greghorne <greghorne@hotmail.com>
2018-10-16 15:20:03 +0000

    remove constants

--- app/assets/javascripts/constants.js
+++ app/assets/javascripts/constants.js
@@ -18,9 +18,9 @@ const CONST_OSM_ADDR_DETAILS    =  1;
 const CONST_PIN_ANCHOR = new L.Point(48/2, 48);
 const CONST_MARKER_ISS = new L.Icon({ iconUrl: "/assets/42598-rocket-icon.png", iconsize: [48, 48], iconAnchor: CONST_PIN_ANCHOR, popupAnchor: [0,-52] });
 
-const CONST_MESSAGE_PROVIDER_CHECKBOX         = "At least one drive-time polygon provider must be selected."
-const CONST_MESSAGE_INVALID_XY                = "Calculations are limited to within U.S. borders."
-const CONST_MESSAGE_UNABLE_TO_REVERSE_GEOCODE = "Unable to reverse geocode location."
+// const CONST_MESSAGE_PROVIDER_CHECKBOX         = "At least one drive-time polygon provider must be selected."
+// const CONST_MESSAGE_INVALID_XY                = "Calculations are limited to within U.S. borders."
+// const CONST_MESSAGE_UNABLE_TO_REVERSE_GEOCODE = "Unable to reverse geocode location."
 
 const CONST_MESSAGE_DISPLAY_TIME              = 10000
 const CONST_MESSAGE_INVALID_XY_DISPLAY_TIME   = 3000
