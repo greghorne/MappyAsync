aaed6a37247767b0c1d1afb057ff47a010520cf0
greghorne <greghorne@hotmail.com>
2018-11-08 19:05:10 +0000

    removed code no longer used

--- app/models/mappyasync.rb
+++ app/models/mappyasync.rb
@@ -1,41 +1,41 @@
-require "resolv"
-require "pg"
-require 'rest-client'
+# require "resolv"
+# require "pg"
+# require 'rest-client'
 # require "redis"
 # require "redis-namespace"
 
 # $redis = Redis::Namespace.new("redis_hostnames", :redis => Redis.new)
 
-class DBConnector
+# class DBConnector
 
-  ########################################
-  def pg_connect
+#   ########################################
+#   def pg_connect
 
-    begin
-      host      = ENV['RAILS_HOST']
-      dbname    = ENV['RAILS_DATABASE']
-      port      = ENV['RAILS_PORT']
-      user      = ENV['RAILS_USERNAME']
-      password  = ENV['RAILS_PASSWORD']
+#     begin
+#       host      = ENV['RAILS_HOST']
+#       dbname    = ENV['RAILS_DATABASE']
+#       port      = ENV['RAILS_PORT']
+#       user      = ENV['RAILS_USERNAME']
+#       password  = ENV['RAILS_PASSWORD']
 
-      conn = PG::Connection.open(
-        :host     => host,
-        :dbname   => dbname,
-        :port     => port,
-        :user     => user,
-        :password => password
-      )
-      return conn
-    rescue
-      return false
-    end
+#       conn = PG::Connection.open(
+#         :host     => host,
+#         :dbname   => dbname,
+#         :port     => port,
+#         :user     => user,
+#         :password => password
+#       )
+#       return conn
+#     rescue
+#       return false
+#     end
 
-  end
-  ########################################
+#   end
+#   ########################################
 
-end
+# end
 
-class Mappyasync
+# class Mappyasync
 
 
-end
+# end
