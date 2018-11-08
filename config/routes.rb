a04918bcfa2966a7c32ae30b207ac664d7b56fa0
greghorne <greghorne@hotmail.com>
2018-11-08 20:38:31 +0000

    changed function name

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "/"               => "mappy_asyncs#mappyasync"
-  get "/mapclick"       => "mappy_asyncs#mapclick"
-  get "/check_valid_xy" => "mappy_asyncs#check_valid_xy"
-  get "/process_xy"     => "mappy_asyncs#process_xy"
+  get "/"                => "mappy_asyncs#mappyasync"
+  get "/mapclick"        => "mappy_asyncs#mapclick"
+  get "/check_valid_xy"  => "mappy_asyncs#check_valid_xy"
+  get "/process_targomo" => "mappy_asyncs#process_targomo"
 
 end
