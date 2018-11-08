2ef8c27e2dd966b02651c8225b673531bb5084ee
greghorne <greghorne@hotmail.com>
2018-11-08 19:05:49 +0000

    removed no longer used code

--- app/controllers/mappy_asyncs_controller.rb
+++ app/controllers/mappy_asyncs_controller.rb
@@ -1,5 +1,7 @@
 require 'rest-client'
 
+targomo_key = ENV['RAILS_TARGOMO']
+
 class MappyAsyncsController < ApplicationController
 
     # def mapclick
@@ -8,15 +10,17 @@ class MappyAsyncsController < ApplicationController
     # end
 
     # check if given x,y falls within U.S. boundaries
-    def check_valid_xy
-        puts "check_valid_xy"
-        valid_xy = Mappyasync.new(params[:lat], params[:lng]).check_valid
-        render :json => { valid: valid_xy } 
-    end
+    # def check_valid_xy
+    #     puts "check_valid_xy"
+    #     valid_xy = Mappyasync.new(params[:lat], params[:lng]).check_valid
+    #     render :json => { valid: valid_xy } 
+    # end
 
     def process_xy
 
         targomo_key = ENV['RAILS_TARGOMO']
+
+        puts targomo_key
 
         url = "http://zotac1.ddns.net:8001/v1/targomo-isochrone/" + params[:lng] + "/" + params[:lat] + "/" + params[:minutes] + "/" + targomo_key
         puts url
