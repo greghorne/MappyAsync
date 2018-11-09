9f5e794198f18343f6fe0fca6ca4399e055feb59
greghorne <greghorne@hotmail.com>
2018-11-09 20:26:16 +0000

    removed no longer used routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/"                => "mappy_asyncs#mappyasync"
-  get "/mapclick"        => "mappy_asyncs#mapclick"
-  get "/check_valid_xy"  => "mappy_asyncs#check_valid_xy"
+  # get "/mapclick"        => "mappy_asyncs#mapclick"
+  # get "/check_valid_xy"  => "mappy_asyncs#check_valid_xy"
   get "/process_targomo" => "mappy_asyncs#process_targomo"
   get "/process_bing"    => "mappy_asyncs#process_bing"
 
