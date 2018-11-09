b57731ad338a82a63e25a5daff678e301be801dd
greghorne <greghorne@hotmail.com>
2018-11-09 20:44:48 +0000

    added route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get "/"                => "mappy_asyncs#mappyasync"
   # get "/mapclick"        => "mappy_asyncs#mapclick"
   # get "/check_valid_xy"  => "mappy_asyncs#check_valid_xy"
-  get "/process_targomo" => "mappy_asyncs#process_targomo"
+
   get "/process_bing"    => "mappy_asyncs#process_bing"
+  get "/process_here"    => "mappy_asysnc#process_here"
+  get "/process_targomo" => "mappy_asyncs#process_targomo"
 
 end
