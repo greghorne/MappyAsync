cf4ad14a472599db8268919fb45322a891aebb1b
greghorne <greghorne@hotmail.com>
2018-11-27 18:53:59 +0000

    added clearIso function

--- app/controllers/mappy_asyncs_controller.rb
+++ app/controllers/mappy_asyncs_controller.rb
@@ -3,22 +3,22 @@ require 'rest-client'
 class MappyAsyncsController < ApplicationController
 
     def process_bing
-        response = RestClient.get "http://zotac1.ddns.net:8002/v1/bing-isochrone/" + params[:lng] + "/" + params[:lat] + "/" + params[:time] + "/" + Rails.application.config.bing_key
+        response = RestClient.get Rails.application.config.api_server + ":8002/v1/bing-isochrone/" + params[:lng] + "/" + params[:lat] + "/" + params[:time] + "/" + Rails.application.config.bing_key
         render :json => "{\"bing\":" + JSON.parse(response)['bing'] + ",\"index\": " + params['index'] + "}"
     end
 
     def process_here
-        response = RestClient.get "http://zotac1.ddns.net:8003/v1/here-isochrone/" + params[:lng] + "/" + params[:lat] + "/" + params[:time] + "/" + Rails.application.config.here_id + "/" + Rails.application.config.here_code
+        response = RestClient.get Rails.application.config.api_server + ":8003/v1/here-isochrone/" + params[:lng] + "/" + params[:lat] + "/" + params[:time] + "/" + Rails.application.config.here_id + "/" + Rails.application.config.here_code
         render :json => "{\"here\":[" + JSON.parse(response)['here'] + "],\"index\": " + params['index'] + "}"
     end
 
     def process_targomo
-        response = RestClient.get "http://zotac1.ddns.net:8001/v1/targomo-isochrone/" + params[:lng] + "/" + params[:lat] + "/" + params[:time] + "/" + Rails.application.config.targomo_key
+        response = RestClient.get Rails.application.config.api_server + ":8001/v1/targomo-isochrone/" + params[:lng] + "/" + params[:lat] + "/" + params[:time] + "/" + Rails.application.config.targomo_key
         render :json => "{\"targomo\":" + JSON.parse(response)['targomo'] + ",\"index\": " + params['index'] + "}"
     end
 
     def process_mapbox
-        response = RestClient.get "http://zotac1.ddns.net:8004/v1/mapbox-isochrone/" + params[:lng] + "/" + params[:lat] + "/" + params[:time] + "/" + Rails.application.config.mapbox_token
+        response = RestClient.get Rails.application.config.api_server + ":8004/v1/mapbox-isochrone/" + params[:lng] + "/" + params[:lat] + "/" + params[:time] + "/" + Rails.application.config.mapbox_token
         render :json => "{\"mapbox\":" + JSON.parse(response)['mapbox'] + ",\"index\": " + params['index'] + "}"
     end
 
